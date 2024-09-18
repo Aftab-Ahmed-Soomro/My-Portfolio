1f310ab26d1f021c327b5e2ba4abdd1fbfedb9eb
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-09-19 01:34:59 +0500

    personal portfolio

--- src/Components/Projectss/Banner4.jsx
+++ src/Components/Projectss/Banner4.jsx
@@ -5,11 +5,11 @@ const Banner4 = ({data, isrotate}) => {
     console.log(data);
   return (
     <div className='bg-black'>
-        <div className='py-[60px] px-[80px]'>
+        <div className='py-[60px] md:px-[80px]'>
             <div className='px-[32px]'>
-                <div className={`flex md:flex-row xsm:flex-col  pt-10 ${isrotate && 'md:flex-row-reverse'}`}>
-                    <div className='md:w-1/2 xsm:w-[343px] xsm:h-[397.73px] xsm:m-auto'>
-                        <a href={hostLink} target='_blank'><img className='h-[350px] rounded' src={imgUrl} alt="" /></a>
+                <div className={`flex md:flex-row xsm:flex-col justify-center items-center pt-10 ${isrotate && 'md:flex-row-reverse'}`}>
+                    <div className='md:w-1/2 xsm:m-auto'>
+                        <a href={hostLink} target='_blank'><img className='md:h-[350px] md:w-[530px] xsm:w-[343px] xsm:h-[397.73px] rounded' src={imgUrl} alt="" /></a>
                     </div>
                     <div className='md:w-1/2 xsm:w-[343px] xsm:h-[168px] md:h-[265px] xsm:mb-28 xsm:m-auto p-8 flex flex-col gap-7'>
                         <h1 className='font-extrabold text-5xl text-white'>{heading}</h1>
